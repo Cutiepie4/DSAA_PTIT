7ea950eeb391565c2d034f17599bc4509b3f1e8c
cozyuptou <trviet4302@gmail.com>
2022-05-07 22:35:50 +0700

    test

---
+++ abcxyz.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string chuyen2to16(string s) {
+    while (s.size() % 4 != 0) s.insert(s.begin(), '0');
+}
+
+void dfs() {
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int M[100][100];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> M[i][j];
+
+}
