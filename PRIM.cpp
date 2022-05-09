32866cc5531b7e6c59e2b396d0dfd01888ca4bfe
cozyuptou <trviet4302@gmail.com>
2022-05-09 17:40:08 +0700

    DSAA

--- PRIM.cpp
+++ PRIM.cpp
@@ -1,69 +1,72 @@
-/*
-vector kq chứa cạnh
-while lặp khi chưa đủ V đỉnh:
-    tìm e = (u,v) có ts nhỏ nhất mà u thuộc kq, v ko thuộc kq
-    if (tìm đc e) {
-        bổ sung cạnh và đỈnh, trọng số
-    }
-    else return ko có cây khung
-    return kq
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int>II;
+
+struct node {
+    int w, v;
+    node(int a, int b) {
+        w = a, v = b;
+    }
+};
 
 int n, m;
-vector <int> chuaxet;
-vector <vector <II>> List;
-set <II> S;
-II canh[1005];
-long long ans = 0;
+vector <vector <node>> M;
+vector <int> Prv;
 
-void Prim() {
-    int i, u, v;
+int Prim(int st = 1) {
+    int ans = 0;
+    set <node> S;
+    vector <node> D(n + 1); // Data
+    vector <int> mark(n + 1); // đánh 
+
+    for (int i = 1; i <= n; i++) {
+        D[i] = node(1e9, i);
+    }
+    Prv[st] = st;
+    D[st] = node(0, st);
+    for (int i = 1; i <= n; i++) {
+        S.insert(D[i]);
+    }
+
     while (!S.empty()) {
-        u = S.begin()->second;
-        int d = S.begin()->first;
-        chuaxet[u] = 1;
-        ans += d;
+        int u = S.begin()->v;
+        mark[u] = 0;
+        ans += S.begin()->w;
         S.erase(S.begin());
-        for (i = 0; i < List[u].size(); i++) {
-            v = List[u][i].second;
-            d = List[u][i].first;
-            // cập nhật lại danh sách cạnh
-            if (chuaxet[v] == 0 && canh[v].first > d) {
-                S.erase(canh[v]);
-                canh[v].first = d;
-                S.insert(canh[v]);
+
+        for (auto i : M[u]) {
+            int v = i.v, w = i.w;
+            if (mark[v] && D[v].w > w) {
+                S.erase(D[v]);
+                D[v].w = w;
+                S.insert(D[v]);
+                Prv[v] = u;
             }
         }
     }
+    return ans;
 }
 
-main() {
-    int t, i, u, v, c;
+void solution() {
+    int st;
+    cin >> n >> m >> st;
+    M = vector <vector <node>>(n + 1);
+    Prv = vector <int>(n + 1, 0);
+
+    for (int i = 0, u, v, w; i < m; i++) {
+        cin >> u >> v >> w;
+        M[u].push_back({ w,v });
+        M[v].push_back({ w,u });
+    }
+    cout << Prim(st);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
     cin >> t;
     while (t--) {
-        List = vector <vector <II>>(1005);
-        chuaxet = vector <int>(505, 0);
-        S.clear(); ans = 0;
-        for (i = 1;i <= n;i++) {
-            canh[i].first = 1e9;
-            canh[i].second = i;
-        }
-
-        cin >> n >> m;
-        for (i = 1;i <= m;i++) {
-            cin >> u >> v >> c;
-            List[u].push_back(II(c, v));
-            List[v].push_back(II(c, u));
-        }
-
-        canh[1].first = 0;
-        for (i = 1; i <= n; i++) S.insert(canh[i]);
-        Prim();
-        cout << ans << endl;
+        solution();
+        cout << endl;
     }
 }
-
