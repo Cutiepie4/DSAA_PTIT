6095827c8828809a88e5024ae0a3726dc286ce18
cutiepie4 <trviet4302@gmail.com>
2022-08-19 22:36:03 +0700

    Testing repo

--- BIẾN ĐỔI A-B.cpp
+++ BIẾN ĐỔI A-B.cpp
@@ -4,7 +4,6 @@ Cho xâu ký tự s chỉ bao gồm hai chữ cái là ‘A’ và ‘B’.
 Mỗi bước được phép biến đổi một vị trí bất kỳ trong xâu (A thành B, B thành A) hoặc cũng có thể biến đổi một dãy liên tiếp các ký tự nào đó tính từ đầu xâu.
 
 Hãy tính xem cần ít nhất bao nhiêu bước để biến đổi xâu về dạng toàn chữ cái A.
-
 Input
 
 Chỉ có 1 dòng ghi xâu ký tự s, độ dài không quá 1 triệu ký tự.  
